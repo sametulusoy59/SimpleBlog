76b72b17c0cdbba38d4415dd01601b1d2f46adf4
SametUlusoy <samet-ulusoy@hotmail.com>
2017-02-23 17:54:37 +0200

    second commit

---
+++ SimpleBlog/SimpleBlog/Areas/admin/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Areas.admin.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: admin/User
+        public ActionResult Index()
+        {
+            return Content("Admin area users controller ındex action");
+        }
+        public ActionResult List()
+        {
+            return Content("Admin area users controller list action");
+        }
+        public ActionResult edit(int id=0)
+        {
+            return Content("Admin area users controller edit action with parameter : "+id.ToString());
+        }
+    }
+}
